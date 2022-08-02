f2fdbc0bb9ec2541561008201e72bddc3244e49f
Timahvey12 <timofei.plaksitskiy@gmail.com>
2022-08-02 02:09:49 +0200

    New styles

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ import { ingr, orderState } from "./constans.js";
 
 const test = document.getElementById("test");
 test.addEventListener("change", ({ target }) => {
-
-  
-  console.log(target.checked);
-  if(target.name === 'main_base' || target.name === 'sauce'){
+  // console.log(target.checked);
+  if (target.name === "main_base" || target.name === "sauce") {
     orderState[target.name][0] = ingr[target.name][target.value];
-  }else{
-    if(target.checked){
-      if(orderState[target.name].length < 2){
+
+    // function eddTagIngr() {
+    //   const order_abaut = document.querySelector("order_abaut");
+    //   console.log(order_abaut);
+    //   const newDiv = order_abaut.innerHTML;
+    //   newDiv.innerHTML = `<p>${orderState[target.name][0]}</p>`;
+    //   return newDiv;
+    // }
+  } else {
+    if (target.checked) {
+      if (orderState[target.name].length < 2) {
         orderState[target.name].push(ingr[target.name][target.value]);
-      }else{
+      } else {
         target.checked = false;
       }
-    }else{
-      const idx = orderState[target.name].indexOf(ingr[target.name][target.value])
+    } else {
+      const idx = orderState[target.name].indexOf(
+        ingr[target.name][target.value]
+      );
       orderState[target.name].splice(idx, 1);
-      
     }
   }
-  console.log(orderState);
+  console.log(orderState[target.name]);
 });
